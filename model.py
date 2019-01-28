474b0eb3bd6d4dd7351fa0b519c3f3459859b4e0
rjritik1137 <rjain@ec.iitr.ac.in>
2019-01-29 00:16:23 +0530

    third commit

--- model.py
+++ model.py
@@ -40,7 +40,7 @@ def model(train_X,train_y,test_X,test_y,learning_rate,epoch,batch_size):
                 epoch_cost=epoch_cost+minibatch_cost/num_mini
 #            if epoch%100==0:
             print("Cost after epoch %i: %f" %(epoch,epoch_cost))
-            saver.save(sess,"E:/spider/Fashion_mnist/mnist.ckpt")  
+            #saver.save(sess,"E:/spider/Fashion_mnist/mnist.ckpt")  
             if epoch%5==0:
                 costs.append(epoch_cost)
         plt.plot(np.squeeze(costs))
@@ -52,7 +52,7 @@ def model(train_X,train_y,test_X,test_y,learning_rate,epoch,batch_size):
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
         print ("Train Accuracy:", accuracy.eval({X: train_X, Y: train_y}))
         print ("Test Accuracy:", accuracy.eval({X: test_X, Y: test_y}))   
-#        saver.save(sess,"E:/spider/Fashion_mnist/mnist.ckpt")            
+        saver.save(sess,"E:/spider/Fashion_mnist/mnist.ckpt")            
     return None
     
 
@@ -60,21 +60,29 @@ def model(train_X,train_y,test_X,test_y,learning_rate,epoch,batch_size):
 
 def network(X,krnls):
     inputs=tf.cast(tf.reshape(X,[-1,28,28,1]),tf.float32)    
-    conv1=tf.nn.relu(tf.nn.conv2d(inputs,krnls["1"],padding="SAME",strides=[1,1,1,1])+krnls["b1"])
+    conv1=tf.nn.relu(tf.nn.conv2d(inputs,krnls["1"],padding="SAME",strides=[1,1,1,1],name='conv1'))
+    pool1=tf.layers.max_pooling2d(inputs=conv1,pool_size=[2,2],strides=[2,2],name='pool1')
+    b_norm1=tf.nn.relu(tf.layers.batch_normalization(inputs=pool1,momentum=0.99,center=True,scale=True,epsilon=0.000001,name="b_norm1"))
 #    conv1=tf.layers.conv2d(inputs,filters=32,kernel_size=[3,3],strides=1,padding='same',activation='relu')
-    pool1=tf.layers.max_pooling2d(inputs=conv1,pool_size=[2,2],strides=[2,2])
+    
 #    conv2=tf.layers.conv2d(inputs=pool1,filters=64,kernel_size=[3,3],strides=1,padding='same',activation='relu')
-    conv2=tf.nn.relu(tf.nn.conv2d(pool1,krnls["2"],padding="SAME",strides=[1,1,1,1])+krnls["b2"])    
-    pool2=tf.layers.max_pooling2d(inputs=conv2,pool_size=[2,2],strides=[2,2])
+    conv2=tf.nn.relu(tf.nn.conv2d(b_norm1,krnls["2"],padding="SAME",strides=[1,1,1,1],name='conv2'))
+    pool2=tf.layers.max_pooling2d(inputs=conv2,pool_size=[2,2],strides=[2,2],name='pool2')
+    b_norm2=tf.nn.relu(tf.layers.batch_normalization(inputs=pool2,momentum=0.99,center=True,scale=True,epsilon=0.000001,name="b_norm2"))
+    
 #    conv3=tf.layers.conv2d(inputs=pool2,filters=128,kernel_size=[3,3],strides=1,padding='same',activation='relu')
-    conv3=tf.nn.relu(tf.nn.conv2d(pool2,krnls["3"],padding="SAME",strides=[1,1,1,1])+krnls["b3"])
-    pool3=tf.layers.max_pooling2d(inputs=conv3,pool_size=[2,2],strides=[2,2])
+    conv3=tf.nn.relu(tf.nn.conv2d(b_norm2,krnls["3"],padding="SAME",strides=[1,1,1,1],name='conv3'))
+    pool3=tf.layers.max_pooling2d(inputs=conv3,pool_size=[2,2],strides=[2,2],name='pool3')
+    b_norm3=tf.nn.relu(tf.layers.batch_normalization(inputs=pool3,momentum=0.99,center=True,scale=True,epsilon=0.000001,name="b_norm3"))
+    
     
     flat=tf.reshape(pool3,[-1,pool3.shape[1]*pool3.shape[2]*pool3.shape[3]])
-    dense1=tf.layers.dense(inputs=flat,units=512,activation='relu',name="dense1")
-    dropout1=tf.layers.dropout(inputs=dense1,rate=0.5,name="dropout1")
-    dense2=tf.layers.dense(inputs=dropout1,units=256,activation='relu',name="dense2")
-    dropout2=tf.layers.dropout(inputs=dense2,rate=0.5,name="dropout2")
+    dense1=tf.layers.dense(inputs=flat,units=512,activation=None,name="dense1")
+    b_norm4=tf.nn.relu(tf.layers.batch_normalization(inputs=dense1,momentum=0.99,scale=True,center=True,epsilon=0.000001,name="b_norm4"))
+    dropout1=tf.layers.dropout(inputs=b_norm4,rate=0.5,name="dropout1")
+    dense2=tf.layers.dense(inputs=dropout1,units=256,activation=None,name="dense2")
+    b_norm5=tf.nn.relu(tf.layers.batch_normalization(inputs=dense2,momentum=0.99,scale=True,center=True,epsilon=0.000001,name="b_norm5"))
+    dropout2=tf.layers.dropout(inputs=b_norm5,rate=0.5,name="dropout2")
     logits=tf.layers.dense(inputs=dropout2,units=10,name="dense3")
     
     return logits
@@ -93,8 +101,8 @@ def initialize_kernels(channels):
     parameter=dict()
     for i in range(1,L):
         parameter[str(i)]=tf.get_variable(name=str(i),shape=[3,3,channels[i-1],channels[i]],dtype=tf.float32,initializer=tf.contrib.layers.xavier_initializer())
-        parameter["b"+str(i)]=tf.get_variable(name="b"+str(i),shape=[channels[i]],dtype=tf.float32,initializer=tf.zeros_initializer())
-        print(parameter[str(i)].shape,parameter["b"+str(i)].shape)
+        #parameter["b"+str(i)]=tf.get_variable(name="b"+str(i),shape=[channels[i]],dtype=tf.float32,initializer=tf.zeros_initializer())
+        print(parameter[str(i)].shape)
         
     return parameter
 
